9d8d29a081ea3f25ecfe7180b317e7906c9667b9
David Martin <demartins37@yahoo.com>
2019-06-28 23:49:47 -0400

    submitting before midnight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,14 @@ var eplTeams = ["arsenal", "villa", "bournemouth", "brighton", "burnley", "chels
  "everton", "leicester", "liverpool", "city", "united", "newcastle",
 "norwich", "sheffield", "southampton", "tottenham", "watford", "westham ", "wolves"];
 var letterBank = ("abcdefghijklmnopqrstuvwxyz").split("");
-var currentWord = "";
+var currentWord = eplTeams[Math.floor(Math.random() * eplTeams.length)];
 var underScores = [];
-
-
-var lettersInCurrentWord = [];
+var currentMessage = "";
+var keyPressed = "";
   
 var alreadyGuessed = [];
-var winCounter = 0
-var numberOfGuesses = 7
+var winCounter = 0;
+var numberOfGuesses = 8;
 
 
 
@@ -20,39 +19,87 @@ var numberOfGuesses = 7
   // put letterGuessed in alreadyGuessed[] and subtract 1 from numberOfGuesses
 //}
 
-function intializeVariables(){
-  currentWord = eplTeams[Math.floor(Math.random() * eplTeams.legth)];
-  numberOfGuesses = 7;
-  
-  for (i = 0; i < currentWord.length; i++); {
-    underScores[i] = "_";
-  }
-  lettersInCurrentWord = currentWord.split("");
-  //console.log(currentWord);
+function intializeVariables() {
+  alreadyGuessed = [];
+  numberOfGuesses = 8;
+  currentWord = eplTeams[Math.floor(Math.random() * eplTeams.length)];
+  makeUnderScores();
 }
+function makeUnderScores() {
+  underScores = [];
+  for (i = 0; i < currentWord.length; i++) {
+    underScores.push("_");
+   
+  }
+}
+ 
+
+function wordCheck() {
+  var letterMatch = false;
+  for (i = 0; i < currentWord.length; i++) {
+    if(currentWord[i] === keyPressed) {
+      underScores[i] = keyPressed;
+      letterMatch = true;
+     for(j = 0; j < underScores.length; j++) {
+       if(underScores[j] === 0) {
+
+       };
+      
+     }
+  }
+  }
+  if(!letterMatch) {
+    numberOfGuesses--;
+  }
+}
+function numberOfGuessesCheck() {
+  if(numberOfGuesses <= 0) {
+    intializeVariables();
+    currentMessage = "Game Over ... Press Key to Start";
+    repaintDOM();
+  }
+}
+function keyClicked(event) {
+ // console.log("you clicked " + event.key);
+  keyPressed = event.key;
+  if (alreadyGuessed.indexOf(keyPressed) === -1) {
+    currentMessage = "Already Guessed " + keyPressed;
+    alreadyGuessed.push(keyPressed);
+    wordCheck();
+    numberOfGuessesCheck();
+    repaintDOM();
+  }else {
+    currentMessage = "Try Another Letter";
+    repaintDOM();
+  }
+} 
 function repaintDOM () {
   var underScoresElement = document.getElementById("underScores");
   var guessesRemainingElement = document.getElementById("guessesRemaining");
   var alreadyGuessedElement = document.getElementById("alreadyGuessed");
   var winsElement = document.getElementById("wins");
+  var userMessageElement = document.getElementById("userMessage");
   underScoresElement.textContent = "Current Word: " + underScores.join(" ");
   guessesRemainingElement.textContent = "Guesses Remaining: " + numberOfGuesses;
-  alreadyGuessedElement.textContent = "Already Guessed: " + alreadyGuessed + letterBank[alreadyGuessed];
+  alreadyGuessedElement.textContent = "Already Guessed: " + alreadyGuessed;
   winsElement.textContent = "Wins: " + winCounter;
+  userMessageElement.textContent = currentMessage;
 }  
+function setEventListeners() {
+  document.onkeyup = keyClicked;
+}
 function startGame() {
   // initialize variables
 
   // create my underscores
   // repaint the dom
 
-  document.onkeyup = keyClicked
-}
-function keyClicked(event) {
-  console.log("you clicked " + event.key);
   
+  intializeVariables();
+  setEventListeners();
   repaintDOM();
-} 
+}
+
    
 
 startGame();
